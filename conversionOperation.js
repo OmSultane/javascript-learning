c99104ced728de17c66e7b21e477f94148212831
Om Sultane <sultaneom256@gmail.com>
2025-12-23 13:02:11 +0530

    First commit: conversion operation

---
+++ conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "om sultane";
+
+console.log(score);
+
+let scoreInNumber = Number(score)
+
+console.log(scoreInNumber);
+
+
+/*
+    We can convert values/datatypes to number using Number(parameter)  
+    But...
+    "33" => 33
+    "33abc" => NaN
+    true => 1; false => 0
+*/
+
+
+
+//***************** Operations ***************** */
+
---
+++ first.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+
+ 
+let name = "om sultane";
+const age = 18;
+const city="jalgaon";
+
+// x=2;    //not allow
+
+
+// console.table([name,age,city]);
+
+console.log(typeof null);
