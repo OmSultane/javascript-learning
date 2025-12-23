4336ac31a4e1b3922c003d5430536dd04141c670
Om Sultane <sultaneom256@gmail.com>
2025-12-23 19:26:54 +0530

    Basic string methods

---
+++ strings.js
@@ -0,0 +1,13 @@
+const name="om"
+
+const age = 18
+
+console.log(`hello my name is ${name} and my age is ${age}`);
+
+let n = new String('Om-Sultane') //we can create string using this constructor also it provides so "many methods" that's the reason of using it
+
+console.log(`the length of name ${n} is ${n.indexOf('S')}`);
+
+console.log(n.includes('-'))
+
+console.log(n.split('-'));
