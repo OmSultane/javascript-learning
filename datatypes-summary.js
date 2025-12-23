cda82dc121ac0b605ff2c3307ca7fdeff8a9f8ad
Om Sultane <sultaneom256@gmail.com>
2025-12-23 15:14:57 +0530

    Types of data types and symbol method

---
+++ datatypes-summary.js
@@ -0,0 +1,24 @@
+/* There are two types of data types 
+
+    Primitive:
+
+        7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+    Reference (Non primitive)
+
+        Array, Objects, Functions
+
+*/
+
+//for String Number Boolean Null undefined already written in conversionOperation.js
+
+//--------------------Symbol----------------------
+const id = Symbol('123');
+
+const anotherId = Synbol('123')
+
+console.log(id === anotherId);    // o/p : false  Symbol returns different values that's the reason of symbol method use
+
+
+
