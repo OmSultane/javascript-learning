ac79bc3d19ceef0ea3e32a7bae66d1ca21c5bacc
Om Sultane <sultaneom256@gmail.com>
2025-12-29 18:26:54 +0530

    do-while loop and cointue & break theory

--- loops.js
+++ loops.js
@@ -1,13 +1,13 @@
+// -----------------------for loop---------------------------
+// let array = [1, 2, 3, 4, 5]
 
-let array = [1, 2, 3, 4, 5]
-
-for (let i = 0; i < array.length; i++) {
-    let element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     let element = array[i];
     
-    // console.log(element);
-}
+//     // console.log(element);
+// }
 
-// console.log(i);
+// // console.log(i);
 
 
 for(let i = 1; i<=25; i++){
@@ -19,4 +19,28 @@ for(let i = 1; i<=25; i++){
             console.log(`${i} x ${j} = ${i*j}`); 
         }
     }
+}
+
+/*
+    continue & break keyword
+    continue - if the condition still the loop runs but it skips current action for that condition
+    break - if the condition occurs it breaks the loop 
+*/
+
+
+
+// ------------------------------do-while loop--------------------------------
+let val = 10;
+
+while(val<=10){
+    console.log(`the value of index is ${val}`);
+    val = val+2;
+    
+}
+
+let players = ["Rohit", "virat", "sachin"]
+let i=0
+while(i <= players.length){
+    console.log(players[i]);
+    i++;
 }
